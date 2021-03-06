07065759a99e8cf13db919fa75f595446bf4fc3b
trung <trung@10.1.10.35>
2021-03-06 12:30:54 -0600

    just update some minor

--- GP1/src/Transaction.java
+++ GP1/src/Transaction.java
@@ -1,4 +1,42 @@
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.List;
 
-public class Transaction {
+public class Transaction implements Serializable {
+	// Variables for Transaction
+	private Member member;
+	private List<Product> products = new LinkedList<Product>();
+	private Calendar date;
+
+	public Transaction(Member member, List<Product> products, Calendar date) {
+		this.member = member;
+		this.products = products;
+		this.date = date;
+	}
+
+	public Member getMember() {
+		return member;
+	}
+
+	public void setMember(Member member) {
+		this.member = member;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+
+	public Calendar getDate() {
+		return date;
+	}
+
+	public void setDate(Calendar date) {
+		this.date = date;
+	}
 
 }
