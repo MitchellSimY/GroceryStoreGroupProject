806812b6220a8a61776aabcadcbb07f0321d9c61
Mitchell Young <mitchellsimy@gmail.com>
2021-03-08 20:44:44 -0600

    Incorrect Singleton classes.
    I made memberList and productList as their own classes when this
    was first created. I ended up understanding the basic concepts
    of singletons and I've since deleted those classes.
    
    If im correct they'll both go in GroceryStore as Singletons.

--- GP1/src/GroceryStore.java
+++ GP1/src/GroceryStore.java
@@ -1,4 +1,11 @@
-
+/**
+ * Mitch: I made memberList and productList as their own classes when this 
+ * was first created. I ended up understanding the basic concepts
+ * of singletons and I've since deleted those classes.
+ * 
+ * If im correct they'll both go in here as Singletons.
+ *
+ */
 public class GroceryStore {
 
 }
