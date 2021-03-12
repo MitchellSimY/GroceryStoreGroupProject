49964917242cbf8b9833f2caf5153ca948021d21
Kou <Kou@KY>
2021-03-12 16:45:40 -0600

    Edited Order/OrderList class

--- GP1/src/business/entities/Order.java
+++ GP1/src/business/entities/Order.java
@@ -4,7 +4,11 @@ import java.io.Serializable;
 public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private int orderID;
-	private String productID;
-	private int quantityOrder;
-
+	private Product reorderProduct;
+	private int quantityOrdered;
+	private static final int counter = 0;
+	
+	public Order(Product productToOrder, )
+	
+	
 }
