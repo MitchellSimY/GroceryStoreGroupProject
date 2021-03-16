3b5a3658ee1441c0a0cb061bc53621ab64bb7605
Kou <Kou@KY>
2021-03-15 23:05:26 -0500

    Updated Order, OrderList class

--- GP1/src/business/collections/OrderList.java
+++ GP1/src/business/collections/OrderList.java
@@ -5,15 +5,23 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
-
 import business.entities.Order;
+
 public class OrderList implements Iterable<Order>, Serializable{
 	private List<Order> orders = new LinkedList<Order>();
-
-	@Override
-	public Iterator<Order> iterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 	
+    /**
+     * Adds an Order object to the list.
+     * 
+     * @param hold the Hold object to be added
+     */
+    public boolean addHold(Order newOrder) {
+    	orders.add(newOrder);
+        return true;
+    }
+
+    @Override
+    public Iterator<Order> iterator() {
+        return orders.iterator();
+    }
 }
--- GP1/src/business/entities/Order.java
+++ GP1/src/business/entities/Order.java
@@ -1,14 +1,43 @@
 package business.entities;
 import java.io.Serializable;
+import java.util.Calendar;
 
 public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private int orderID;
 	private Product reorderProduct;
 	private int quantityOrdered;
-	private static final int counter = 0;
+	private Calendar date;
+	private static int counter = 0;
 	
-	public Order(Product productToOrder, )
+	public Order(Product productToOrder, Calendar date) {
+		orderID = counter++;
+		quantityOrdered = productToOrder.getReorderLevel()*2;
+		reorderProduct = productToOrder;
+		this.date = date;
+	}
+
+	public int getOrderID() {
+		return orderID;
+	}
+
+	public Product getReorderProduct() {
+		return reorderProduct;
+	}
+
+	public int getQuantityOrdered() {
+		return quantityOrdered;
+	}
+	
+	public String getDate() {
+		return date.get(Calendar.MONTH) + "/" + date.get(Calendar.DATE) + "/" + date.get(Calendar.YEAR);
+	}
+
+	@Override
+	public String toString() {
+		return "OrderID: " + orderID + "\t" + reorderProduct.getProductName() + "\tQty Ordered: "
+				+ getDate() + "\t" + quantityOrdered;
+	}
 	
 	
 }
